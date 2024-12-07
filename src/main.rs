54c091469495fd96f12c2902649069654660f007
Sergey Mamadjanov <sm@drdow.org>
2024-12-07 22:53:30 +0500

    adds day-1 exercise: implementation of how todolist should handle simple commands like 'add', 'list' and 'exit'

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    println!("-- Welcome to To-Do app! --");
+    println!("Please, enter a command (add, list, exit):");
+    io::stdout().flush().unwrap();
+
+    let mut command: String = String::new();
+    io::stdin().read_line(&mut command)
+        .expect("Failed to read command");
+
+    match command.trim() {
+        "add" => {
+            println!("Adding a new entry (NOT IMPLEMENTED)");
+        },
+        "list" => {
+            println!("Listing all available entries (NOT IMPLEMENTED)");
+        },
+        "exit" => {
+            println!("Exiting...");
+            return;
+        },
+        _ => {
+            println!("Invalid command");
+        }
+    }
 }
